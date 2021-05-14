183c0336ec943e492e01c8bd430dcf27a0c4e658
KostiaShulgin <kavshul@gmail.com>
2021-05-14 20:38:25 +0300

    1

--- resources/views/LandingDesktop.blade.php
+++ resources/views/LandingDesktop.blade.php
@@ -5,46 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hardware Tech</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/LandingDesktop.css') }}" >
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/Сarousel.css') }}" >
-    <script>
-        /* Устанавливаем индекс слайда по умолчанию */
-        let slideIndex = 1;
-        showSlides(slideIndex);
-
-        /* Увеличиваем индекс на 1 — показываем следующий слайд*/
-        function nextSlide() {
-            showSlides(slideIndex += 1);
-        }
-
-        /* Уменьшает индекс на 1 — показываем предыдущий слайд*/
-        function previousSlide() {
-            showSlides(slideIndex -= 1);
-        }
-
-        /* Устанавливаем текущий слайд */
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-
-        /* Функция перелистывания */
-        function showSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("item");
-
-            if (n > slides.length) {
-                slideIndex = 1
-            }
-            if (n < 1) {
-                slideIndex = slides.length
-            }
-
-            /* Проходим по каждому слайду в цикле for */
-            for (let slide of slides) {
-                slide.style.display = "none";
-            }
-            slides[slideIndex - 1].style.display = "block";
-        }
-    </script>
 </head>
 <body>
 
@@ -93,28 +53,28 @@
             <p>Відеокарти</p>
             <hr>
         </div>
-        <!-- Основной блок слайдера -->
-        <div class="slider">
-
-            <!-- Первый слайд -->
-            <div class="item">
-                <img src="https://s3.tproger.ru/uploads/2020/07/field.jpg">
+        <div id="slider-box">
+            <div id="slider">
+                <figure>
+                    <div>
+                        <img src="{{ asset('images/search.svg') }}" alt>
+                    </div>
+                    <div>
+                        <img src="{{ asset('images/search.svg') }}" alt>
+                    </div>
+                    <div>
+                        <img src="{{ asset('images/search.svg') }}" alt>
+                    </div>
+                    <div>
+                        <img src="{{ asset('images/search.svg') }}" alt>
+                    </div>
+                    <div>
+                        <img src="{{ asset('images/search.svg') }}" alt>
+                    </div>
+                </figure>
             </div>
-
-            <!-- Второй слайд -->
-            <div class="item">
-                <img src="https://s3.tproger.ru/uploads/2020/07/rose.jpg">
-            </div>
-
-            <!-- Третий слайд -->
-            <div class="item">
-                <img src="https://s3.tproger.ru/uploads/2020/07/leaf.jpg">
-            </div>
-
-            <!-- Кнопки-стрелочки -->
-            <a class="previous" onclick="previousSlide()">&#10094;</a>
-            <a class="next" onclick="nextSlide()">&#10095;</a>
         </div>
+
         <div id="more-items">
 
         </div>
